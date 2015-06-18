47c9ac89ccafaf111e92da5c646e4e875c75211f
josemartin81 <jose_etsi@hotmail.com>
2015-06-18 14:01:34 +0200

    página de créditos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,12 @@
 //GET /quizes/question
 exports.question = function(req, res) {
+  res.render('quizes/creditos', {autor: 'Jose María Martín',
+     foto: 'https://avatars1.githubusercontent.com/u/4822240?v=3&s=460'});
+};
+
+
+//GET /quizes/question
+exports.question = function(req, res) {
   res.render('quizes/question', {pregunta: 'Capital de Italia'});
 };
 
