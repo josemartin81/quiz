7a5d04dd029052b978151c817fb23ee2e04f3e8a
josemartin81 <jose_etsi@hotmail.com>
2015-06-18 14:26:07 +0200

    Revisión primera de los créditos. Funciona!

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,5 @@
-//GET /quizes/question
-exports.question = function(req, res) {
+//GET /quizes/creditos
+exports.creditos = function(req, res) {
   res.render('quizes/creditos', {autor: 'Jose María Martín',
      foto: 'https://avatars1.githubusercontent.com/u/4822240?v=3&s=460'});
 };
